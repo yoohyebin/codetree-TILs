6c8f6df0443d39067c72b524b9ed64562a1e15c3
Hyebin (Helia) <hyebin218@naver.com>
2024-01-09 16:15:41 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240109/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.swift
@@ -0,0 +1,15 @@
+func solution(_ a: [Int], _ b: [Int]) -> String {
+    for i in 0..<(a.count - b.count) {
+        if Array(a[i..<i+b.count]) == b {
+            return "Yes"
+        }
+    }
+
+    return "No"
+}
+
+let _ = readLine()!.split(separator: " ").map{Int(String($0))!}
+let a = readLine()!.split(separator: " ").map{Int(String($0))!}
+let b = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+print(solution(a, b))
