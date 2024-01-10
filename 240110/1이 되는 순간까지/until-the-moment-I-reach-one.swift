c31803d77f597cfda496216e284123cdbabcd23a
Hyebin (Helia) <hyebin218@naver.com>
2024-01-10 15:34:57 +0900

    Title: 1이 되는 순간까지, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/1이 되는 순간까지/until-the-moment-I-reach-one.swift
@@ -0,0 +1,13 @@
+func solution(_ n: Int) -> Int {
+    if n == 1 {
+        return 0
+    }
+    if n%2 == 0 {
+        return solution(n/2)+1
+    }
+    else {
+        return solution(n/3)+1
+    }
+}
+
+print(solution(Int(readLine()!)!))
