da19e42f4a6e898c6ca23f48cceb20b2db310636
Hyebin (Helia) <hyebin218@naver.com>
2024-01-11 14:06:58 +0900

    Title: 줄 세우기, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/줄 세우기/line-up-students.swift
@@ -0,0 +1,35 @@
+class Student {
+    let number: Int
+    let height: Int
+    let weight: Int
+    
+    init(number: Int, height: Int, weight: Int) {
+        self.number = number
+        self.height = height
+        self.weight = weight
+    }
+}
+
+let n = Int(readLine()!)!
+var students = [Student]()
+
+for i in 0..<n {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    students.append(Student(number: i+1, height: input[0], weight: input[1]))
+}
+
+students.sort{
+    if $0.height == $1.height {
+        if $0.weight == $1.weight {
+            return $0.number < $1.number
+        } else {
+            return $0.weight > $1.weight
+        }
+    } else {
+        return $0.height > $1.height
+    }
+}
+
+for student in students {
+    print(student.height, student.weight, student.number)
+}
