6580c14ed0995f8b052ff28df6fda3878f383cb6
Hyebin (Helia) <hyebin218@naver.com>
2024-01-11 13:10:56 +0900

    Title: 2개씩 그룹짓기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/2개씩 그룹짓기/group-of-pairs.swift
@@ -0,0 +1,9 @@
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
+var maxValue = 0
+
+for i in 0..<n {
+    maxValue = max(maxValue, arr[i]+arr[2*n-1-i])
+}
+
+print(maxValue)
