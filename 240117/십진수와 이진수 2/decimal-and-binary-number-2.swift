8e46b2693b86a9ced06f9a2c1f7b30df1c3e2fbb
Hyebin (Helia) <hyebin218@naver.com>
2024-01-17 20:02:38 +0900

    Title: 십진수와 이진수 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240117/십진수와 이진수 2/decimal-and-binary-number-2.swift
+++ 240117/십진수와 이진수 2/decimal-and-binary-number-2.swift
@@ -1,3 +1,17 @@
-let input = readLine()!
-let result = (Int(input, radix: 2) ?? 0)*17
-print(String(result, radix: 2))
+let input = readLine()!.map{Int(String($0))!}
+var temp = 0, n = 1
+
+for i in input.reversed() {
+    temp += i*n
+    n*=2
+}
+
+temp *= 17
+var result = ""
+
+while temp > 0 {
+    result += "\(temp%2)"
+    temp /= 2
+}
+
+print(String(result.reversed()))
