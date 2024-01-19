3972816a19e1a8e2e864c67750194f7a0396ce11
Hyebin (Helia) <hyebin218@naver.com>
2024-01-19 19:46:35 +0900

    Title: 사각형의 총 넓이 2, Time: 9ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240119/사각형의 총 넓이 2/total-width-of-a-rectangle2.swift
@@ -0,0 +1,23 @@
+var arr = Array(repeating: Array(repeating: 0, count: 201), count: 201)
+let n = Int(readLine()!)!
+var sum = 0
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let (x1, y1, x2, y2) = (input[0], input[1], input[2], input[3])
+    
+    sum += (x2-x1)*(y2-y1)
+    
+    for i in x1...x2 {
+        for j in y1...y2 {
+            arr[i+100][j+100] += 1
+        }
+    }
+}
+
+var doubleWidth = 0
+for i in 0..<arr.count {
+    doubleWidth += arr[i].filter{$0 == 2}.count
+}
+
+print(sum-doubleWidth+5)
