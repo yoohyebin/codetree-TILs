88567421f268a685107a6625cdddfcf905e0bcc2
Hyebin (Helia) <hyebin218@naver.com>
2024-01-19 20:04:18 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 10ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240119/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.swift
@@ -0,0 +1,25 @@
+var arr = Array(repeating: Array(repeating: 0, count: 2001), count: 2001)
+
+for n in 0..<2 {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    let (x1, y1, x2, y2) = (input[0], input[1], input[2], input[3])
+    
+    for i in x1..<x2 {
+        for j in y1..<y2 {
+            arr[i+1000][j+1000] = n+1
+        }
+    }
+}
+
+var x = 0, y = 0
+for i in 0..<arr.count {
+    let n = arr[i].filter{$0 == 1}.count
+    
+    if n != 0 {
+        x += 1
+        y = max(y, n)
+    }
+    
+}
+
+print(x*y)
