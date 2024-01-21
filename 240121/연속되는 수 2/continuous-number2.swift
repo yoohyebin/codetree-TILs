f1b150baf0148d7cc5dcdd303cc53668621ab2bc
Hyebin (Helia) <hyebin218@naver.com>
2024-01-21 13:13:40 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/연속되는 수 2/continuous-number2.swift
@@ -0,0 +1,16 @@
+let n = Int(readLine()!)!
+var prev = -1
+var cnt = 0, result = 0
+
+for _ in 0..<n {
+    let a = Int(readLine()!)!
+    if prev == a {
+        cnt += 1
+    } else {
+        prev = a
+        result = max(cnt, result)
+        cnt = 1
+    }
+}
+
+print(result)
