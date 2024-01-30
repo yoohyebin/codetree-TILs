3104d31de4af90747136b3fb992a162a622ef112
Hyebin (Helia) <hyebin218@naver.com>
2024-01-30 19:59:04 +0900

    Title: 되돌아오기 2, Time: 14ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240130/되돌아오기 2/come-back-2.swift
@@ -0,0 +1,25 @@
+let dx = [0, 1, 0, -1]
+let dy = [1, 0, -1, 0]
+
+let input = readLine()!.map{String($0)}
+var (x, y) = (0, 0)
+var result = -1, cnt = 0
+var index = 0
+
+for s in input {
+    cnt += 1
+    
+    if s == "F" {
+        (x,y) = (x+dx[index], y+dy[index])
+    } else if s == "L" {
+        index = (3+index)%4
+    } else {
+        index = (index+1)%4
+    }
+    
+    if x == 0, y == 0 {
+        result = cnt
+    }
+}
+
+print(result)
