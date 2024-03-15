eb6897e02940c9c324f043c9bf49d983f14e9a02
Hyebin (Helia) <hyebin218@naver.com>
2024-03-15 15:52:06 +0900

    Title: 최고의 13위치 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240315/최고의 13위치 2/best-place-of-13-2.swift
@@ -0,0 +1,26 @@
+let n = Int(readLine()!)!
+var arr = [[Int]]()
+var result = 0
+
+for _ in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+for i in 0..<n {
+    for j in 0..<n-2 {
+        for k in 0..<n{
+            for l in 0..<n-2{
+                if i == k && l >= j && l < j+3 {
+                    continue
+                }
+                
+                var sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                sum += arr[k][l] + arr[k][l+1] + arr[k][l+2]
+                
+                result = max(result, sum)
+            }
+        }
+    }
+}
+
+print(result)
