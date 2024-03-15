392f0c1e5881106b72fa890e36934bf1d270782a
Hyebin (Helia) <hyebin218@naver.com>
2024-03-15 15:04:06 +0900

    Title: Carry 피하기 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240315/Carry 피하기 2/escaping-carry-2.swift
+++ 240315/Carry 피하기 2/escaping-carry-2.swift
@@ -1,6 +1,6 @@
 let n = Int(readLine()!)!
 var arr = [Int]()
-var result = 0
+var result = -1
 
 for _ in 0..<n {
     arr.append(Int(readLine()!)!)
@@ -10,11 +10,11 @@ for i in 0..<n-2 {
     for j in i+1..<n-1 {
         for k in j+1..<n {
             var(a,b,c) = (arr[i], arr[j], arr[k])
-            var flag = true
+            var carry = true
             
-            while (a != 0 && b != 0 && c != 0) {
+            for _ in 0..<4 {
                 if a%10 + b%10 + c%10 >= 10 {
-                    flag = false
+                    carry = false
                     break
                 }
                 a/=10
@@ -22,7 +22,7 @@ for i in 0..<n-2 {
                 c/=10
             }
             
-            if flag {
+            if carry {
                 result = max(result, arr[i]+arr[j]+arr[k])
             }
         }
