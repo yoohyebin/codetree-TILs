2448525b870cf606125d91c9e74627fbf9303d5f
Hyebin (Helia) <hyebin218@naver.com>
2024-03-19 21:01:17 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/한 가지로 열리는 자물쇠/one-way-lock.swift
+++ 240319/한 가지로 열리는 자물쇠/one-way-lock.swift
@@ -6,12 +6,11 @@ var result = 0
 for i in 1...n {
     for j in 1...n {
         for k in 1...n {
-            if abs(a-i) <= 2 || abs(b-j) <= 2 || abs(c-k) <= 2 {
+            if abs(arr[0]-i) <= 2 || abs(arr[1]-j) <= 2 || abs(arr[2]-k) <= 2 {
                 result += 1
             }
         }
     }
 }
-
 
 print(result)
