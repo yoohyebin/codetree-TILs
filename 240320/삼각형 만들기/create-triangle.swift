48cf0d16f7d7503c1a22ad387746c92a6657ec94
Hyebin (Helia) <hyebin218@naver.com>
2024-03-20 15:25:31 +0900

    Title: 삼각형 만들기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/삼각형 만들기/create-triangle.swift
+++ 240320/삼각형 만들기/create-triangle.swift
@@ -13,8 +13,10 @@ for i in 0..<n-2 {
             let (x1,y1) = (arr[i].x, arr[i].y)
             let (x2,y2) = (arr[j].x, arr[j].y)
             let (x3,y3) = (arr[k].x, arr[k].y)
-            
-            result = max(result, abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)))
+            if (x1 == x2 || x1 == x3 || x2 == x3) && 
+                (y1 == y2 || y1 == y3 || y2 == y3) {
+                result = max(result, abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)))
+            }
         }
     }
 }
