ab624487b98164a1e5cfdc89c1f73b0b9176a400
Hyebin (Helia) <hyebin218@naver.com>
2024-03-20 15:19:28 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/가장 가까운 두 점 사이의 거리/closest-pair-of-points.swift
@@ -0,0 +1,23 @@
+let n = Int(readLine()!)!
+var arr = [(x: Int, y: Int)]()
+var minDiff = Int.max
+var result = 0
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    arr.append((input[0], input[1]))
+}
+
+for i in 0..<n-1 {
+    for j in i+1..<n {
+        let (x1, y1) = (arr[i].x, arr[i].y)
+        let (x2, y2) = (arr[j].x, arr[j].y)
+        
+        if minDiff > abs(x1-x2) + abs(y1-y2) {
+            minDiff = abs(x1-x2) + abs(y1-y2)
+            result = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
+        }
+    }
+}
+
+print(result)
