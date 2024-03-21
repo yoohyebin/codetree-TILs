a8d2b162669b51725d19583e2101d40db9d02459
Hyebin (Helia) <hyebin218@naver.com>
2024-03-21 17:08:46 +0900

    Title: 겹치지 않는 선분 2, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.swift
@@ -0,0 +1,22 @@
+let n = Int(readLine()!)!
+var arr = [(x1: Int, x2: Int)]()
+var result = n
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    arr.append((input[0], input[1]))
+}
+
+arr.sort(by: {
+    $0.x1 < $1.x1
+})
+
+for i in 0..<n-1 {
+    for j in i+1..<n {
+        if arr[j].x1 >= arr[i].x1 && arr[j].x1 < arr[i].x2 {
+            result -= 1
+        }
+    }
+}
+
+print(result)
