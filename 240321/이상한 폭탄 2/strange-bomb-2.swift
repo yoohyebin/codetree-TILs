909417e12e42cdc27503393c82cd39789cff82ad
Hyebin (Helia) <hyebin218@naver.com>
2024-03-21 18:16:17 +0900

    Title: 이상한 폭탄 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/이상한 폭탄 2/strange-bomb-2.swift
@@ -0,0 +1,22 @@
+let nk = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (n,k) = (nk[0], nk[1])
+var arr = Array(repeating: [Int](), count: 1001)
+var result = 0
+
+for i in 0..<n {
+    arr[Int(readLine()!)!].append(i)
+}
+
+for b in 0..<1001 {
+    if arr[b].isEmpty {
+        continue
+    }
+    
+    for idx in 0..<arr[b].count-1 {
+        if arr[b][idx+1] - arr[b][idx] <= k {
+            result = b
+        }
+    }
+}
+
+print(result)
