343e90f96fcb65e5eddd2fad9734e6f946c59eec
Hyebin (Helia) <hyebin218@naver.com>
2024-03-29 17:24:59 +0900

    Title: 팰린드롬 수 찾기, Time: 810ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/팰린드롬 수 찾기/find-the-number-of-palindrome.swift
+++ 240329/팰린드롬 수 찾기/find-the-number-of-palindrome.swift
@@ -2,9 +2,17 @@ let xy = readLine()!.split(separator: " ").map{Int(String($0))!}
 var result = 0
 
 for n in xy[0]...xy[1] {
-    if String(n) == String(String(n).reversed()) {
-        result += 1
+    var flag = true
+    var number = String(n).map{Int(String($0))!}
+    
+    for j in 0..<number.count/2{
+        if number[j] != number[number.count-j-1] {
+            flag = false
+            break
+        }
     }
+        
+    result += flag ? 1 : 0
 }
 
 print(result)
