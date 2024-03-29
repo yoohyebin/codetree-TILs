8b0566ee65bd4aba2de2acb1c01473847fd29fa8
Hyebin (Helia) <hyebin218@naver.com>
2024-03-29 17:08:55 +0900

    Title: 등차수열, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/등차수열/arithmetic-sequence.swift
+++ 240329/등차수열/arithmetic-sequence.swift
@@ -3,13 +3,16 @@ let arr = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
 var result = 0
 
 for i in 0..<arr.max()! {
+    var cnt = 0
     for j in 0..<n-1 {
         for k in j+1..<n {
             if abs(i-arr[j]) == abs(i-arr[k]) {
-                result += 1
+                cnt += 1
             }
         }
     }
+    
+    result = max(result, cnt)
 }
 
 print(result)
