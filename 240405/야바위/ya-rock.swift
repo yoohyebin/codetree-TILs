a9bf69490929ae3faf5aa9ac7e634836e7e1357b
Hyebin (Helia) <hyebin218@naver.com>
2024-04-05 13:20:02 +0900

    Title: 야바위, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240405/야바위/ya-rock.swift
@@ -0,0 +1,22 @@
+let n = Int(readLine()!)!
+var arr = [[Int]]()
+var result = 0
+
+for _ in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+for i in 1...n {
+    var score = 0
+    var yabawi = Array(repeating: false, count: 4)
+    yabawi[i] = true
+    
+    for a in arr {
+        yabawi.swapAt(a[0], a[1])
+        score += yabawi[a[2]] ? 1 : 0
+    }
+    
+    result = max(result, score)
+}
+
+print(result)
