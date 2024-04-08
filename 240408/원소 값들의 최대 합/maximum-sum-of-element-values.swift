1b5aea04f5d4d50fc878589f9a570968197223eb
Hyebin (Helia) <hyebin218@naver.com>
2024-04-08 21:36:21 +0900

    Title: 원소 값들의 최대 합, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/원소 값들의 최대 합/maximum-sum-of-element-values.swift
@@ -0,0 +1,18 @@
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (n,m) = (input[0], input[1])
+let arr = [0]+readLine()!.split(separator: " ").map{Int(String($0))!}
+var result = 0
+
+for i in 1...n {
+    var sum = arr[i]
+    var current = arr[i]
+    
+    for _ in 0..<m-1 {
+        sum += arr[current]
+        current = arr[current]
+    }
+    
+    result = max(result, current)
+}
+
+print(result)
