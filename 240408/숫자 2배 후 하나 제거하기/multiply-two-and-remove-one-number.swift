a91483d4bafd2704f9865fe93b15d5881aab624b
Hyebin (Helia) <hyebin218@naver.com>
2024-04-08 20:32:07 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.swift
@@ -0,0 +1,28 @@
+let n = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var result = Int.max
+
+for i in 0..<n {
+    arr[i] *= 2
+    
+    for j in 0..<n {
+        var cnt = 0
+        var prev = -1
+        
+        for k in 0..<n {
+            if k == j {continue}
+            
+            if prev != -1 {
+                cnt += abs(arr[k] - prev)
+            }
+            
+            prev = arr[i]
+        }
+        
+        result = min(result, cnt)
+    }
+    
+    arr[i] /= 2
+}
+
+print(result)
