310bb6a1f43ad830ea8f44b70b30d8be331a77d6
Hyebin (Helia) <hyebin218@naver.com>
2024-04-08 20:40:34 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.swift
+++ 240408/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.swift
@@ -2,15 +2,12 @@ let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (a,b,c) = (input[0], input[1], input[2])
 var result = 0
 
-for i in 0..<c {
+for i in 0...c/a {
     var sum = a*i
+
+    let num_b = (c-sum)/b
     
-    if sum > c {break}
-    
-    for j in 0..<c {
-        if sum+b > c {break}
-        sum += b
-    }
+    sum += num_b*b
     
     result = max(result, sum)
 }
