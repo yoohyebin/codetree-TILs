0081f62ef2044778863b43f4f70797ee855de78c
Hyebin (Helia) <hyebin218@naver.com>
2024-04-20 14:14:48 +0900

    Title: 두 선분, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240420/두 선분/two-lines.swift
+++ 240420/두 선분/two-lines.swift
@@ -1,7 +1,7 @@
 let x = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (x1, x2, x3, x4) = (x[0], x[1], x[2], x[3])
 
-if x3 >= x1 && x2 <= x2 {
+if x3 >= x1 && x3 <= x2 {
     print("intersecting")
 } else if x4 >= x1 && x4 <= x2 {
     print("intersecting")
