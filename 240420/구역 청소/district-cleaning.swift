2819a66e0477e83444b846af8f245cc027515b4a
Hyebin (Helia) <hyebin218@naver.com>
2024-04-20 14:26:23 +0900

    Title: 구역 청소, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240420/구역 청소/district-cleaning.swift
+++ 240420/구역 청소/district-cleaning.swift
@@ -2,12 +2,12 @@ var arr = Array(repeating: 0, count: 101)
 let ab = readLine()!.split(separator: " ").map{Int(String($0))!}
 let cd = readLine()!.split(separator: " ").map{Int(String($0))!}
 
-for i in ab[0]...ab[1] {
+for i in ab[0]..<ab[1] {
     arr[i] = 1
 }
 
-for i in cd[0]...cd[1] {
+for i in cd[0]..<cd[1] {
     arr[i] = 1
 }
 
-print(arr.filter{$0 == 1}.count-1)
+print(arr.filter{$0 == 1}.count)
