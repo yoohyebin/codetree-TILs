cac3e31c9a403e96b1788cd2b746435d37452877
Hyebin (Helia) <hyebin218@naver.com>
2024-04-20 14:22:22 +0900

    Title: 두 직사각형, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240420/두 직사각형/two-rectangles.swift
+++ 240420/두 직사각형/two-rectangles.swift
@@ -3,10 +3,8 @@ let (x1, y1, x2, y2) = (x[0], x[1], x[2], x[3])
 let a = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (a1, b1, a2, b2) = (a[0], a[1], a[2], a[3])
 
-if x1 <= a1 && a1 <= x2 && y1 <= b2 && b2 <= y2 {
-    print("overlapping")
-} else if a1 <= x1 && x1 <= a2 && b1 <= y2 && y2 <= b2 {
-    print("overlapping")
-} else {
+if x2 < a1 || a2 < x1 || b2 < y1 || y2 < b1 {
     print("nonoverlapping")
+} else {
+    print("overlapping")
 }
