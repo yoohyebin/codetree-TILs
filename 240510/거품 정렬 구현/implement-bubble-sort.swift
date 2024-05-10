c65fca1e4c3f8bfb380d2e179bdbd90af35cb88c
Hyebin (Helia) <hyebin218@naver.com>
2024-05-10 11:50:27 +0900

    Title: 거품 정렬 구현, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240510/거품 정렬 구현/implement-bubble-sort.swift
@@ -0,0 +1,15 @@
+let n = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+for i in 0..<n-2 {
+    for j in 0..<n-1 {
+        if arr[j] > arr[j+1] {
+            let temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+        }
+    }
+}
+
+
+print(arr.map{String($0)}.joined(separator: " "))
