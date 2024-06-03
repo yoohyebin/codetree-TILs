820c1fdff084612adab68909dc454a8ef26b4bc5
Hyebin (Helia) <hyebin218@naver.com>
2024-06-03 15:13:04 +0900

    Title: 트로미노, Time: 12ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240603/트로미노/tromino.swift
+++ 240603/트로미노/tromino.swift
@@ -11,15 +11,15 @@ for i in 0..<n {
     for j in 0..<n {
         var maxValue = 0
         
-        if j+3 <= n {
+        if j+2 < m {
             maxValue = max(maxValue, arr[i][j..<j+3].reduce(0, +))
         }
         
-        if i+3 <= n {
+        if i+2 < n {
             maxValue = max(maxValue, arr[i][j]+arr[i+1][j]+arr[i+2][j])
         }
         
-        if i+1 < n, j+1 < n {
+        if i+1 < n, j+1 < m {
             maxValue = max(maxValue, arr[i][j]+arr[i+1][j]+arr[i+1][j+1])
             maxValue = max(maxValue, arr[i][j+1]+arr[i+1][j+1]+arr[i+1][j+1])
             maxValue = max(maxValue, arr[i][j]+arr[i][j+1]+arr[i+1][j])
