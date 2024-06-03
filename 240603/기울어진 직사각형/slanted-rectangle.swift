0516c0739fc84c513b04ac2f9f919a4db2dc2017
Hyebin (Helia) <hyebin218@naver.com>
2024-06-03 15:49:10 +0900

    Title: 기울어진 직사각형, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240603/기울어진 직사각형/slanted-rectangle.swift
@@ -0,0 +1,38 @@
+let n = Int(readLine()!)!
+var arr = [[Int]]()
+var result = 0
+
+let dx = [1, -1, -1, 1]
+let dy = [-1, -1, 1, 1]
+
+for _ in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+
+func solution(_ r: Int, _ c: Int) -> Int {
+    var sum = 0
+    var nr = r, nc = c
+    
+    for i in 0..<4 {
+        if nr+dx[i] >= 0, nr+dx[i] < n, nc+dy[i] >= 0, nc+dy[i] < n  {
+            while nr+dx[i] >= 0, nr+dx[i] < n, nc+dy[i] >= 0, nc+dy[i] < n {
+                sum += arr[nr][nc]
+                nr += dx[i]
+                nc += dy[i]
+            }
+        } else {
+            return 0
+        }
+    }
+    
+    return sum
+}
+
+for i in 0..<n {
+    for j in 0..<n {
+        result = max(result, solution(i, j))
+    }
+}
+
+print(result)
