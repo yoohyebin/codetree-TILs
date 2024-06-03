422ff288076128f7e08ee2cc54b600fe2e28ca77
Hyebin (Helia) <hyebin218@naver.com>
2024-06-03 16:02:40 +0900

    Title: 기울어진 직사각형, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240603/기울어진 직사각형/slanted-rectangle.swift
+++ 240603/기울어진 직사각형/slanted-rectangle.swift
@@ -2,8 +2,8 @@ let n = Int(readLine()!)!
 var arr = [[Int]]()
 var result = 0
 
-let dx = [1, -1, -1, 1]
-let dy = [-1, -1, 1, 1]
+let dx = [-1, -1, 1, 1]
+let dy = [1, -1, -1, 1]
 
 for _ in 0..<n {
     arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
@@ -16,10 +16,14 @@ func solution(_ r: Int, _ c: Int) -> Int {
     
     for i in 0..<4 {
         if nr+dx[i] >= 0, nr+dx[i] < n, nc+dy[i] >= 0, nc+dy[i] < n  {
-            while nr+dx[i] >= 0, nr+dx[i] < n, nc+dy[i] >= 0, nc+dy[i] < n {
-                sum += arr[nr][nc]
-                nr += dx[i]
-                nc += dy[i]
+            while true {
+                if nr+dx[i] >= 0, nr+dx[i] < n, nc+dy[i] >= 0, nc+dy[i] < n {
+                    sum += arr[nr][nc]
+                    nr += dx[i]
+                    nc += dy[i]
+                } else {
+                    break
+                }
             }
         } else {
             return 0
