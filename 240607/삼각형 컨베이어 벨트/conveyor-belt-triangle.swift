4b9dd65bcd81b85065cc8ca1649b9d3f4d870aae
Hyebin (Helia) <hyebin218@naver.com>
2024-06-07 15:46:52 +0900

    Title: 삼각형 컨베이어 벨트, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/삼각형 컨베이어 벨트/conveyor-belt-triangle.swift
+++ 240607/삼각형 컨베이어 벨트/conveyor-belt-triangle.swift
@@ -1,19 +1,19 @@
 let nt = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (n,t) = (nt[0], nt[1])
-var first = readLine()!.split(separator: " ").map{String($0)}.joined()
-var second = readLine()!.split(separator: " ").map{String($0)}.joined()
-var third = readLine()!.split(separator: " ").map{String($0)}.joined()
+var first = readLine()!.split(separator: " ").map{Int(String($0))!}
+var second = readLine()!.split(separator: " ").map{Int(String($0))!}
+var third = readLine()!.split(separator: " ").map{Int(String($0))!}
 
 for _ in 0..<t {
-    var temp1 = String(first.removeLast())
-    var temp2 = String(second.removeLast())
-    var temp3 = String(third.removeLast())
+    var temp1 = first[n-1]
+    var temp2 = second[n-1]
+    var temp3 = third[n-1]
     
-    first = temp3 + first
-    second = temp1 + second
-    third = temp2 + third
+    first = [temp3] + first[0...n-2]
+    second = [temp1] + second[0...n-2]
+    third = [temp2] + third[0...n-2]
 }
 
-print(first.split(separator: "").joined(separator: " "))
-print(second.split(separator: "").joined(separator: " "))
-print(third.split(separator: "").joined(separator: " "))
+print(first.map{String($0)}.joined(separator: " "))
+print(second.map{String($0)}.joined(separator: " "))
+print(third.map{String($0)}.joined(separator: " "))
