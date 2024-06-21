188a2bca50d321064837d84b0df12ac730b740f4
Hyebin (Helia) <hyebin218@naver.com>
2024-06-21 12:04:08 +0900

    Title: 떨어지는 1자 블록, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240621/떨어지는 1자 블록/falling-horizontal-block.swift
@@ -0,0 +1,18 @@
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (n, m, k) = (input[0], input[1], input[2]-1)
+var arr = [[Int]]()
+
+for _ in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+for i in 0..<n-1 {
+    if arr[i+1][k..<k+m].contains(1) {
+        arr[i] = arr[i][0..<k] + Array(repeating: 1, count: m) + arr[i][k+m..<n]
+        break
+    }
+}
+
+for i in 0..<n {
+    print(arr[i].map{String($0)}.joined(separator: " "))
+}
