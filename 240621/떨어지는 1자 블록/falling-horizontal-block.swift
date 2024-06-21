46b33b9d10963fd0a6c9080f99f80f4140f7f241
Hyebin (Helia) <hyebin218@naver.com>
2024-06-21 12:07:00 +0900

    Title: 떨어지는 1자 블록, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240621/떨어지는 1자 블록/falling-horizontal-block.swift
+++ 240621/떨어지는 1자 블록/falling-horizontal-block.swift
@@ -1,6 +1,7 @@
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (n, m, k) = (input[0], input[1], input[2]-1)
 var arr = [[Int]]()
+var flag = true
 
 for _ in 0..<n {
     arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
@@ -9,14 +10,15 @@ for _ in 0..<n {
 for i in 0..<n-1 {
     if arr[i+1][k..<k+m].contains(1) {
         arr[i] = arr[i][0..<k] + Array(repeating: 1, count: m) + arr[i][k+m..<n]
+        flag = false
         break
     }
 }
 
-if n == 1 {
-    print(1)
-} else {
-    for i in 0..<n {
-        print(arr[i].map{String($0)}.joined(separator: " "))
-    }
+if flag {
+    arr[n-1] = arr[n-1][0..<k] + Array(repeating: 1, count: m) + arr[n-1][k+m..<n]
+}
+
+for i in 0..<n {
+    print(arr[i].map{String($0)}.joined(separator: " "))
 }
