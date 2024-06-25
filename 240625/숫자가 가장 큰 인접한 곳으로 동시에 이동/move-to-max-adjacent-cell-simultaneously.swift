27b42a445e955c79980cc0862b00218eb2f9e386
Hyebin (Helia) <hyebin218@naver.com>
2024-06-25 13:25:21 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240625/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.swift
+++ 240625/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.swift
@@ -1,3 +1,8 @@
+struct position: Hashable {
+    var x: Int
+    var y: Int
+}
+
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (n,m,t) = (input[0], input[1], input[2])
 
@@ -6,17 +11,17 @@ for _ in 0..<n {
     board.append(readLine()!.split(separator: " ").map{Int(String($0))!})
 }
 
-var marbles = [(x: Int, y: Int)]()
+var marbles = [position]()
 for _ in 0..<m {
     let value = readLine()!.split(separator: " ").map{Int(String($0))!}
-    marbles.append((value[0]-1, value[1]-1))
+    marbles.append(position(x: value[0]-1, y: value[1]-1))
 }
 
 let dx = [-1, 1, 0, 0], dy = [0, 0, -1, 1]
 
 for _ in 0..<t {
-    var newMarbles = [(x: Int, y: Int)]()
-    
+    var newMarbles = [position: Int]()
+     
     for marble in marbles {
         var max = 0
         var (mx, my) = (0,0)
@@ -28,13 +33,10 @@ for _ in 0..<t {
                 (mx, my) = (nx, ny)
             }
         }
-        
-        if !newMarbles.contains(where: {$0.x == mx && $0.y == my}) {
-            newMarbles.append((mx,my))
-        }
+        newMarbles[position(x: mx, y: my), default: 0] += 1
     }
     
-    marbles = newMarbles
+    marbles = Array(newMarbles.filter{$0.value < 2}.keys)
 }
 
 print(marbles.count)
