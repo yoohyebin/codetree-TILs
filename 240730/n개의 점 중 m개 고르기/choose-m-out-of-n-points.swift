c31573e62918a1d8ffd5198ff79f29ccb5060d74
Hyebin (Helia) <hyebin218@naver.com>
2024-07-30 18:20:47 +0900

    Title: n개의 점 중 m개 고르기, Time: 21ms, Memory: 1MB, Status: Passed - CodeTree

--- 240730/n개의 점 중 m개 고르기/choose-m-out-of-n-points.swift
+++ 240730/n개의 점 중 m개 고르기/choose-m-out-of-n-points.swift
@@ -14,8 +14,10 @@ func cal() -> Int {
     var max_dist = 0
     
     for i in 0..<m-1 {
-        let dist = (selected[i][0]-selected[i+1][0])*(selected[i][0]-selected[i+1][0]) + (selected[i][1]-selected[i+1][1])*(selected[i][1]-selected[i+1][1])
-        max_dist = max(max_dist, dist)
+        for j in i+1..<m {
+            let dist = (selected[i][0]-selected[j][0])*(selected[i][0]-selected[j][0]) + (selected[i][1]-selected[j][1])*(selected[i][1]-selected[j][1])
+            max_dist = max(max_dist, dist)
+        }
     }
     
     return max_dist
