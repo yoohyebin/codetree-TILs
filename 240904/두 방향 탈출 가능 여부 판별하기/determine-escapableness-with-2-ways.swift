ee7906746360b7cd8f891f83ef2191b7d6e6402a
Hyebin (Helia) <hyebin218@naver.com>
2024-09-04 15:17:52 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240904/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
@@ -0,0 +1,29 @@
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (n, m) = (nm[0], nm[1])
+var arr = [[Int]]()
+let dx = [-1, 0, 1, 0], dy = [0, -1, 0, 1]
+var result = 0
+var visited = Array(repeating: Array(repeating: false, count: m), count: n)
+
+for _ in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+
+func dfs(_ x: Int, _ y: Int) {
+    if x == n-1, y == m-1 {
+        result = 1
+        return
+    }
+    
+    for i in 0..<4 {
+        let nx = x + dx[i], ny = y + dy[i]
+        
+        if nx >= 0, nx < n, ny >= 0, ny < m, arr[nx][ny] != 1, !visited[nx][ny] {
+            visited[nx][ny] = true
+            dfs(nx, ny)
+        }
+    }
+}
+
+dfs(0, 0)
+print(result)
