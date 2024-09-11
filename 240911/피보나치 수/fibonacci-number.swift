4f1c3ba2a60f32b2b5e3532ad0cd02ccd973babd
Hyebin (Helia) <hyebin218@naver.com>
2024-09-11 15:56:19 +0900

    Title: 피보나치 수, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/피보나치 수/fibonacci-number.swift
+++ 240911/피보나치 수/fibonacci-number.swift
@@ -2,8 +2,10 @@ let n = Int(readLine()!)!
 var fibo = Array(repeating: 0, count: 46)
 fibo[1] = 1
 
-for i in 2...n {
-    fibo[i] = fibo[i-1]+fibo[i-2]
+if n >= 2 {
+    for i in 2...n {
+        fibo[i] = fibo[i-1]+fibo[i-2]
+    }
 }
 
 print(fibo[n])
