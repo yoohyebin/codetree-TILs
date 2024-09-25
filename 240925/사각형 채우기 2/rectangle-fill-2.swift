4ae6947be60f4907df21ad0c6e3a533bbc6b97cf
Hyebin (Helia) <hyebin218@naver.com>
2024-09-25 18:42:43 +0900

    Title: 사각형 채우기 2, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240925/사각형 채우기 2/rectangle-fill-2.swift
@@ -0,0 +1,12 @@
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 1001)
+dp[0] = 1
+dp[1] = 1
+
+if n >= 2 {
+    for i in 2...n {
+        dp[i] = dp[i-2]*2 + dp[i-1]
+    }
+}
+
+print(dp[n])
