352264419c4048b931a97bf66884ec6a342ba0a2
Hyebin (Helia) <hyebin218@naver.com>
2024-09-26 17:05:25 +0900

    Title: 동전 거슬러주기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240926/동전 거슬러주기/coin-change.swift
+++ 240926/동전 거슬러주기/coin-change.swift
@@ -1,13 +1,13 @@
 let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (n, m) = (nm[0], nm[1])
 let coins = readLine()!.split(separator: " ").map{Int(String($0))!}
-var dp = Array(repeating: Int.max, count: m+1)
+var dp = Array(repeating: 10001, count: m+1)
 dp[0] = 0
 
 for i in 1...m {
     for j in 0..<n {
         if i >= coins[j] {
-            dp[i] = min(dp[i], dp[i-coins[j]]+1)
+            dp[i] = min(dp[i], dp[i-coins[j]])
         }
     }
 }
