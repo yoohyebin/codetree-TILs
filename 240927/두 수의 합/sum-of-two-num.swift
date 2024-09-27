4f798e436be9711811f3a657641fa8daeeea2576
Hyebin (Helia) <hyebin218@naver.com>
2024-09-27 13:56:44 +0900

    Title: 두 수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240927/두 수의 합/sum-of-two-num.swift
@@ -0,0 +1,15 @@
+let nk = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (n, k) = (nk[0], nk[1])
+var result = 0
+var dict = [Int: Int]()
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+for i in arr {
+    dict[i, default: 0] += 1
+}
+
+for i in arr {
+    result += ((dict[abs(i - k)] ?? 0) * (dict[i] ?? 0))
+}
+
+print(result/2)
