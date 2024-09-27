dd2ca01a7f50588f07905ff45d7a4fa0f0ac2d70
Hyebin (Helia) <hyebin218@naver.com>
2024-09-27 16:18:36 +0900

    Title: 작지만 큰 숫자, Time: 1098ms, Memory: 6MB, Status: Time Limit Exceed - CodeTree

--- 240927/작지만 큰 숫자/small-but-big-number.swift
+++ 240927/작지만 큰 숫자/small-but-big-number.swift
@@ -1,5 +1,5 @@
 let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
-var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
 var questions = readLine()!.split(separator: " ").map{Int(String($0))!}
 var set = Set<Int>()
 
@@ -9,10 +9,12 @@ for i in arr {
 
 for q in questions {
     var result = -1
-    for i in stride(from: q, through: 1, by: -1) {
-        if set.contains(i) {
-            result = set.remove(i) ?? 0
-            break
+    if arr.first ?? Int.max <= q{
+        for i in stride(from: q, through: 1, by: -1) {
+            if set.contains(i) {
+                result = set.remove(i) ?? 0
+                break
+            }
         }
     }
     print(result)
