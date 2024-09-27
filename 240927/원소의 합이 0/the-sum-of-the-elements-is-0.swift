f97f9100ead600392fa8721dd279891225b91d3f
Hyebin (Helia) <hyebin218@naver.com>
2024-09-27 14:48:13 +0900

    Title: 원소의 합이 0, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240927/원소의 합이 0/the-sum-of-the-elements-is-0.swift
+++ 240927/원소의 합이 0/the-sum-of-the-elements-is-0.swift
@@ -3,33 +3,25 @@ var arrA = readLine()!.split(separator: " ").map{Int(String($0))!}
 var arrB = readLine()!.split(separator: " ").map{Int(String($0))!}
 var arrC = readLine()!.split(separator: " ").map{Int(String($0))!}
 var arrD = readLine()!.split(separator: " ").map{Int(String($0))!}
-var dictA = [Int: Int]()
-var dictB = [Int: Int]()
-var dictC = [Int: Int]()
-var dictD = [Int: Int]()
+
+var dict = [Int: Int]()
+
 var result = 0
 
 for i in 0..<n {
-    dictA[arrA[i], default: 0] += 1
-    dictB[arrB[i], default: 0] += 1
-    dictC[arrC[i], default: 0] += 1
-    dictD[arrD[i], default: 0] += 1
-}
-
-
-for i in 0..<n {
-    dictA[arrA[i], default: 1] -= 1
     for j in 0..<n {
-        dictB[arrB[i], default: 1] -= 1
-        for k in 0..<n {
-            let diff = 0 - (arrA[i] + arrB[j] + arrC[k])
-            
-            if let val = dictD[diff] {
-                result += val
-            }
-        }
+        dict[arrA[i] + arrB[j], default: 0] += 1
     }
 }
 
+for i in 0..<n {
+    for j in 0..<n {
+        let diff = arrC[i] - arrD[j]
+        
+        if let val = dict[diff] {
+            result += val
+        }
+    }
+}
 
 print(result)
