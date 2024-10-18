ec0a59a42718b29e2599b5985dfa63a8f6f619b1
Hyebin (Helia) <hyebin218@naver.com>
2024-10-18 15:13:10 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 271ms, Memory: 21MB, Status: Runtime Error - CodeTree

--- 241018/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.swift
+++ 241018/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.swift
@@ -4,12 +4,12 @@ var pin = Array(repeating: 0, count: 1000001)
 var sumArray = Array(repeating: 0, count: 1000001)
 var results = [String]()
 
-for a in readLine()!.split(separator: " ").map({Int(String($0))!}) {
-    pin[a] = 1
+for i in readLine()!.split(separator: " ").map({Int(String($0))!}) {
+    sumArray[i] += 1
 }
 
-for i in 1..<1000001 {
-    sumArray[i] = sumArray[i-1] + pin[i]
+for i in 1...1000000 {
+    sumArray[i] += sumArray[i-1]
 }
 
 for _ in 0..<q {
